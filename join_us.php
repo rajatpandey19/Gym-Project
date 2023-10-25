b7337d728482f54c601eebf511d4bf2ee8fd1676
rajatpandey19 <rajatpandey1303@gmali.com>
2023-10-25 11:02:05 +0530

    Project summision

---
+++ join_us.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title></title>
+		
+<!-- CSS Bootstrap -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+		
+		
+<!-- CSS Style -->
+		<style>
+				
+		</style>
+		
+		<link rel="stylesheet" href="css/style.css">
+		
+	</head>
+	<body>
+	<div class="container">
+		<h1>Contact Form</h1>
+		<form action="https://formsubmit.co/rajatpandey1303@gmail.com" method="POST">
+			<input type="text" name="Name" placeholder="Full Name" required>
+			<input type="email" name="Email" placeholder="Email" required>
+			<select name="Continent">|
+				<option>Africa</option>
+				<option>Antarctica</option>
+				<option>Asia</option>
+				<option>Australia</option>
+				<option>Europe</option>
+				<option>North America</option>
+				<option>South America</option>
+			</select>
+			<textarea name="Message" placeholder="Message" required></textarea>
+			<button type="submit">Send</button>
+		</form>
+	</div>
+	
+<!-- JS Bootstrap -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+	
+	</body>
+</html>
+
